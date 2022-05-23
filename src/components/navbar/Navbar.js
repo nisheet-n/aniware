a0869705b5099cb5382625b0beb3ec6c04b95aaf
Nisheet <nisheet2000@gmail.com>
2022-05-23 23:54:02 +0530

    Mode Styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import "./Navbar.css"
 
 function Navbar() {
-     const key = "isDarkMode"
-     const [isDark, setIsDark] = useState(() => {
+     const key = "isLightMode"
+     const [isLight, setIsLight] = useState(() => {
           try {
                const item = window.localStorage.getItem(key) // Getting from local storage by key
                return item ? JSON.parse(item) : undefined   // JSON.parse converts from Sting to Boolean
@@ -16,34 +16,34 @@ function Navbar() {
 
      // Check if user has any preference in the local storage.
      // If not then load the system preference
-     const darkModeEnabled = typeof isDark !== "undefined"
-          ? isDark
-          : window.matchMedia("(prefers-color-scheme: dark)").matches
+     const lightModeEnabled = typeof isLight !== "undefined"
+          ? isLight
+          : window.matchMedia("(prefers-color-scheme: light)").matches
 
-     //Dark mode
+     //Adds Light mode to the className
      useEffect(() => {
-          const className = "dark"
-          if (darkModeEnabled) {
+          const className = "light"
+          if (lightModeEnabled) {
                window.document.body.classList.add(className)
           }
           else {
                window.document.body.classList.remove(className)
           }
           try {
-               window.localStorage.setItem(key, darkModeEnabled) // Setting in local storage by key
+               window.localStorage.setItem(key, lightModeEnabled) // Setting in local storage by key
           }
           catch (e) {
                console.error("Error in setting preference")
           }
-     }, [darkModeEnabled])
+     }, [lightModeEnabled])
 
      return (
           <div className="navbar">
                <a href="/" className="navbar-title">AniWare</a>
 
                <div className="nav-links">
-                    {isDark ? (
-                         <svg onClick={() => setIsDark(!isDark)} className="dark-mode" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 385 385">
+                    {isLight ? (
+                         <svg onClick={() => setIsLight(!isLight)} className="dark-mode" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 385 385">
                               <g id="Group_2" data-name="Group 2" transform="translate(-4442 -1720)">
                                    <g id="Group_1" data-name="Group 1" transform="translate(-15 74)">
                                         <rect id="Rectangle_12" data-name="Rectangle 12" width="385" height="385" transform="translate(4457 1646)" fill="none" />
@@ -56,7 +56,7 @@ function Navbar() {
                               </g>
                          </svg>
                     ) : (
-                         <svg onClick={() => setIsDark(!isDark)} className="light-mode" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 385 385">
+                         <svg onClick={() => setIsLight(!isLight)} className="light-mode" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 385 385">
                               <g id="Group_2" data-name="Group 2" transform="translate(-4442 -1720)">
                                    <g id="Group_1" data-name="Group 1" transform="translate(-15 74)">
                                         <rect id="Rectangle_12" data-name="Rectangle 12" width="385" height="385" transform="translate(4457 1646)" fill="none" />
