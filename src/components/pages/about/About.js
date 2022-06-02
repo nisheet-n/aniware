b0e4b248930501c6f102f517996e93905037e1f5
Nisheet <nisheet2000@gmail.com>
2022-06-02 23:59:25 +0530

    About Page Update

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -1,38 +1,22 @@
-import "./About.css";
+import "./About.css"
 
-function About({title, main})
-{
-    return(
-        <div className="aboutcard"> 
-        <div className="about-poster">
-            <div className="title"><h1 >ABOUT </h1></div>
-            <div className="main">
-                <main>
-
-Aniware is a place, where you can  watch new, old, popularly released and latest animes with numerous genres and search functionality  
-
-which will help to classify shows according to your choice and mood :) <br></br>
-<br></br>
-This is an opensource website which we have created using react-js and fetched using API.
-<br></br>
-<br></br>
-We are also working upon Wishlist and Watchlist as the add more feature here.
-<br></br>
-<br></br>
-The aim of this website is that, we can watch advertisement free anime with add-on features.
-<br></br>
-Also you can learn and contribute more to this website, and so we've kept it as an opensource project 
-<br></br>
-<br></br>
-Special Thanks to Riimuru :)
-<br></br>
-<br></br>
-
-Made With Love For Anime Fans ♥ 
-</main>
-</div>
-        </div>
-        </div>
-    )
+function About() {
+	return (
+		<div className="about-page">
+			<h1 className="about-heading">About</h1>
+			<div className="about-content">
+				Aniware is a place, where you can browse popular, new-released and your favourite animes with numerous genres and search functionality.
+				<br /><br />
+				You can also add your animes in the wishlist without even the need of making an account. :)
+				<br /><br />
+				<a href="https://github.com/nisheet-n/aniware" target="_blank" rel="noopener noreferrer">Source link</a>
+				<br />
+				Aniware: Anime Anywhere
+				<br /><br />
+				Made With 💝 For Anime Fans
+			</div>
+		</div >
+	)
 }
-export default About;
+
+export default About
